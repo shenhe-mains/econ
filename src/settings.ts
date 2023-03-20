44d9bf952c5b7a1e31861c2e81d173de10c49599
hyper-neutrino <alex.yj.liao@gmail.com>
2023-03-20 16:43:16 -0400

    adjust settings for test

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,13 @@
 export default {
-    min_delay: 10, // minutes
-    max_delay: 20, // minutes
+    min_delay: 2, // minutes
+    max_delay: 5, // minutes
     min_reduction: 10, // seconds
     max_reduction: 10, // seconds
-    channel: "961464606440718336", // ID
+    channel: "1087476200521343017", // ID
     initial_reward: 100, // event points
     final_reward: 50, // event points
     raid_reward: 25, // event points
     diminish: 10, // percentage,
-    window: 30, // seconds
-    forbidden: ["838128715204263988", "805458032938188838", "805458032938188839"], // ID list
+    window: 60, // seconds
+    forbidden: [], // ID list
 };
